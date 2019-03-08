371e01724dcfd931c72e9a2fe0e2abd0d045addb
Adam Mescher <admescher@gmail.com>
2019-03-08 01:46:54 -0700

    Import Logo and Nav components to abstract logic into respective components

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,28 +1,12 @@
 import React from "react";
+import Logo from "../Logo/index";
+import Nav from "../Nav/index";
 import StyledHeader from "./styled.js";
 
 const Header = () => (
   <StyledHeader>
-    <a href="#">
-      <img
-        src="./assets/logo-crop.svg"
-        alt="Brian Beitzel golf instructor logo"
-        title="Brian Beitzel Golf"
-      />
-    </a>
-    <nav>
-      <ul>
-        <li>
-          <a href="#about">about</a>
-        </li>
-        <li>
-          <a href="#lessons">lessons</a>
-        </li>
-        <li>
-          <a href="#contact">contact</a>
-        </li>
-      </ul>
-    </nav>
+    <Logo />
+    <Nav />
   </StyledHeader>
 );
 
