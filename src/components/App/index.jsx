d6d2e810e99a7d33445c13bf7486060206994ab6
Adam Mescher <admescher@gmail.com>
2019-03-08 01:08:19 -0700

    Wrap application in BrowserRouter

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import StyledApp from "./styled.js";
 import theme from "../../lib/theme";
 
 const App = () => (
-  <ThemeProvider theme={theme}>
-    <StyledApp>
-      <h1>Hello World</h1>
-    </StyledApp>
-  </ThemeProvider>
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <StyledApp>
+        <h1>Hello World</h1>
+      </StyledApp>
+    </ThemeProvider>
+  </BrowserRouter>
 );
 
 export default App;
