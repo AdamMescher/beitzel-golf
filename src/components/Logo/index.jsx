abd48c7728f9a48eb306c2cc4ececf662a6d0253
Adam Mescher <admescher@gmail.com>
2019-03-08 01:41:07 -0700

    Create Logo component and add boilerplate

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import StyledLogo from "./Logo.test";
+
+const Logo = () => (
+  <StyledLogo>
+    <img src="" alt="Beitzel Golf logo" title="Brian Beitzel Golf" />
+  </StyledLogo>
+);
+
+export default Logo;
---
+++ src/components/Logo/styled.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const StyledLogo = styled.header``;
+
+export default StyledLogo;
