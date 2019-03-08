53658f4ff7c6b241cd4a692728b05617b45aedff
Adam Mescher <admescher@gmail.com>
2019-03-08 01:38:04 -0700

    Add pga colors

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,5 +1,48 @@
+/*
+
+Beitzel LOGO FONTS
+- Norwester
+- Quicksand
+
+*/
+
 const theme = {
-  main: "mediumseagreen"
+  pgabrandone: "rgb(180, 151, 90)",
+  pgabrandtwo: "rgb(37, 50, 85)",
+  tpcbrandone: "rgb(0, 62,126)",
+  tpcbrandtwo: "rgb(239, 62, 66)",
+  championsbrandone: "rgb(0, 62, 126)",
+  championsbrandtwo: "rgb(239, 62, 66)",
+  championsbrandthree: "rgb(152, 152, 153)",
+  championscupone: "rgb(0, 160, 223)",
+  championscuptwo: "rgb(66, 85, 99)",
+  championscupthree: "rgb(152, 164, 174)",
+  tpcmarketingdramaone: "rgb(0, 45, 106)",
+  tpcmarketingdramatwo: "rgb(160, 203, 237)",
+  tpcmarketingdramathree: "rgb(204, 216, 229)",
+  tpcmarketingdramafour: "rgb(0, 72, 19)",
+  tpcmarketingdramafive: "rgb(123, 193, 67)",
+  tpcmarketingdramasix: "rgb(215, 195, 0)",
+  tpcmarketingdramaseven: "rgb(245, 232, 130)",
+  tpcmarketingdramaeight: "rgb(244, 125, 48)",
+  tpcmarketingdramanine: "rgb(198, 69, 33)",
+  tpcmarketingdramaten: "rgb(252, 25, 33)",
+  tpcmarketingelementone: "rgb(88, 39, 0)",
+  tpcmarketingelementtwo: "rgb(138, 32, 3)",
+  tpcmarketingelementthree: "rgb(200, 177, 139)",
+  tpcmarketingelementfour: "rgb(220, 205, 168)",
+  tpcmarketingelementfive: "rgb(230, 194, 159)",
+  tpcmarketingelementsix: "rgb(232, 217, 187)",
+  tpcmarketingelementseven: "rgb(255, 236, 188)",
+  tpcmarketingneutralone: "rgb(82, 85, 82)",
+  tpcmarketingneutraltwo: "rgb(209, 205, 197)",
+  tpcmarketingneutralthree: "rgb(237, 235, 231)",
+  tpcmarketingneutralfour: "rgb(0, 0, 0)",
+  tpcmarketingneutralfive: "rgb(101, 102, 104)",
+  tpcmarketingneutralsix: "rgb(128, 130, 133)",
+  tpcmarketingneutralseven: "rgb(153, 155, 158)",
+  tpcmarketingneutraleight: "rgb(179, 181, 184)",
+  tpcmarketingneutralnine: "rgb(199, 200, 202)"
 };
 
 export default theme;
