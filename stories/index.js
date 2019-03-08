007e57c6ea4fa8d8528a4b6aef77ef3ec4a46e07
Adam Mescher <admescher@gmail.com>
2019-03-08 01:12:03 -0700

    Prove that storybook is configured properly

--- stories/index.js
+++ stories/index.js
@@ -1,13 +1,4 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { Button } from "@storybook/react/demo";
-
-storiesOf("Button", module)
-  .add("with text", () => <Button>Hello Button</Button>)
-  .add("with some emoji", () => (
-    <Button>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+import App from "../src/components/App/index";
+storiesOf("App", module).add("Default State", () => <App />);
