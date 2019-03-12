e50521036d1e02ef48b13492b9aa46eb66df15df
Adam Mescher <admescher@gmail.com>
2019-03-11 20:07:42 -0600

    Add Hero boilerplate from template

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -3,7 +3,24 @@ import StyledHero from "./styled";
 
 const Hero = () => (
   <StyledHero>
-    <h1>WE COULD BE HEROS...YOU AND MEEEEEE</h1>
+    <div>
+      <article>
+        <div>
+          <h2>Brian Beitzel</h2>
+          <h2>Golf Professional</h2>
+        </div>
+      </article>
+    </div>
+    <div>
+      <img src="" alt="" />
+    </div>
+    <div>
+      <p>Long irons Dancing on the practise ground.</p>
+      <p>
+        That's a big slice Waggle your hips and swing A controlled draw Struck
+        by lightning You skied it!
+      </p>
+    </div>
   </StyledHero>
 );
 
