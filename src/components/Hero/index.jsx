fd64218324a878bf06c0e9df685c3fa5ecdd2b8d
Adam Mescher <admescher@gmail.com>
2019-03-11 20:42:51 -0600

    Create Hero component with boilerplate

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import StyledHero from './styled';
+
+const Hero = () => <h1>WE COULD BE HEROS...YOU AND MEEEEEE</h1>
+
+export default Hero;
---
+++ src/components/Hero/styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const StyledHero = styled.section``;
+
+export default StyledHero;
